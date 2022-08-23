3b8a6bdcd2805a3300a0f2159449907cb5554e79
hiramramirez <hiram.j.ramirez@gmail.com>
2022-08-22 19:37:38 -0500

    on off and music

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const on = document.querySelector('.on')
 const off = document.querySelector('.off')
+const on = document.querySelector('.on')
+const fondoNegro = document.querySelector('.fondoNegro')
+const audio = new Audio('pokemon.mp3')
+
 
 on.addEventListener('click', () => {
-  on.classList.add('hidden')
+  on.classList.toggle('hidden')
+  fondoNegro.classList.toggle('hidden')
+  off.classList.toggle('hidden')
+  audio.play()
+})
+off.addEventListener('click', () => {
+  off.classList.toggle('hidden')
+  fondoNegro.classList.toggle('hidden')
+  on.classList.toggle('hidden')
 })
